7943ee9f2a0bef56fdef7ccdbb4f6b9ec38fc546
Sam Johnson <samj@iconcollective.com>
2020-06-20 10:43:27 -0700

    added dummy dates route for server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static(__dirname + '../../public'));
 
+app.get('/dates', (data) => {
+    //this is an example route to test 
+})
+
 app.listen(8888, () => {
     console.log('server listening on port 8888!')
 })
