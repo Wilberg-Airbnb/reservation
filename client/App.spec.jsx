ceb50c74f2c249ca2b07ff7696a8aaadd833f323
Sam Johnson <samj@iconcollective.com>
2020-06-25 01:25:34 -0700

    outlined first integartion testing for App's api call

--- client/App.spec.jsx
+++ client/App.spec.jsx
@@ -7,7 +7,6 @@ import axios from "axios";
 configure({ adapter: new Adapter() });
 
 describe('<App />', () => {
-
     it('sets listingId state based on url and calls componentDidMount', () => {
 
       global.window = Object.create(window);
@@ -19,10 +18,31 @@ describe('<App />', () => {
       })
 
       const spy = jest.spyOn(App.prototype, 'componentDidMount');
-      const wrapper = mount(<App />);
+      const wrapper = shallow(<App />);
       expect(wrapper.state().listingId).toEqual(27);
       expect(spy).toHaveBeenCalled();
       spy.mockReset();
       spy.mockRestore();
     })
   });
+
+
+  // describe('API', () => {
+
+  //   it('should return data packet according to listingId when mounted', async () => {
+  
+  //     global.window = Object.create(window);
+  //     const url = "http://localhost:8888/57";
+  //     Object.defineProperty(window, 'location', {
+  //       value: {
+  //         href: url
+  //       }
+  //     })
+  
+  //     const wrapper = await shallow(<App/>)
+  //     // console.log(wrapper.state())
+  
+  //   })
+  
+  
+  // })
