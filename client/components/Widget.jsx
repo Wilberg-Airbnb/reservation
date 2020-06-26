ede628838ff9c30bd12ce58b5d8d911e8b891c9e
Sam Johnson <samj@iconcollective.com>
2020-06-26 15:17:57 -0700

    added unit test coverage for Widget

--- client/components/Widget.jsx
+++ client/components/Widget.jsx
@@ -4,18 +4,24 @@ import React from 'react';
 class Widget extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      listingId: this.props.listingData.listingId
+    };
   }
 
-  componentDidMout() {
-    console.log(this.props)
+  handleClick() {
+    
+  }
+
+  componentDidMount() {
+    // console.log('widget inherits listing data: ', this.props.listingData)
   }
 
   render() {
     return (
       <div>
         <div id="widget-heading">
-          <h3 id="night-price">$70 / night</h3>
+          <h3 id="night-price">${this.props.listingData.standardPrice} / night</h3>
           <span id="avg-rating">⭐️ 4.90 (323)</span>
         </div>
         <div id="calendar-picker">
@@ -25,6 +31,9 @@ class Widget extends React.Component {
             <h5>GUESTS</h5>
             <div>1 guest</div>
           </div>
+        </div>
+        <div>
+          <button id="check-available" onClick={(e) => this.handleClick(e)}>Check availability</button>
         </div>
       </div>
     )
