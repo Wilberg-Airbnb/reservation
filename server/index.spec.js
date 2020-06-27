69f2a4367ca6b2899049694d32d48bf6cb263a73
Sam Johnson <samj@iconcollective.com>
2020-06-26 23:15:03 -0700

    complete unit testing for /api/reservation/:listingId

--- server/index.spec.js
+++ server/index.spec.js
@@ -23,3 +23,44 @@ describe('/:listingId', () => {
   })
 
 })
+
+describe('/api/reservation/:listingId', () => {
+
+  it('should respond with a JSON object and status of 200 if given a listingId', async (done) => {
+    const res = await request.get('/api/reservation/45');
+
+    expect(res.body).toBeDefined();
+    expect(typeof res.body).toBe('object');
+    expect(res.status).toBe(200);
+
+    done()
+  })
+
+  it('should respond with a 404 for listingIds not within 0-99', async (done) => {
+    const res = await request.get('/api/reservation/123');
+
+    expect(res.status).toBe(404);
+
+    done()
+  })
+
+  it('should respond with a 500 when server fails', async (done) => {
+    const res = await request.get('/api/reservation/notAnId')
+
+    expect(res.status).toBe(500);
+    done()
+  })
+
+  it('should respond with a JSON object with availableDates & refundable boolean if given a listingId', async (done) => {
+    const res = await request.get('/api/reservation/45');
+
+    expect(res.body).toBeDefined();
+    expect(res.body).toHaveProperty('availableDates');
+    expect(typeof res.body.refundable).toBe('boolean');
+    expect(res.status).toBe(200);
+
+    done()
+  })
+
+
+})
