be8ab11935281d806c9343e28dd3862b7a366a88
Sam Johnson <samj@iconcollective.com>
2020-07-03 13:43:20 -0700

    added swireframing & some styles for first phase of booking widget

--- client/components/Widget.jsx
+++ client/components/Widget.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 // import React from 'react-dom';
 
 class Widget extends React.Component {
@@ -10,7 +11,7 @@ class Widget extends React.Component {
   }
 
   handleClick() {
-    
+
   }
 
   componentDidMount() {
@@ -19,25 +20,80 @@ class Widget extends React.Component {
 
   render() {
     return (
-      <div>
-        <div id="widget-heading">
-          <h3 id="night-price">${this.props.listingData.standardPrice} / night</h3>
-          <span id="avg-rating">⭐️ 4.90 (323)</span>
-        </div>
-        <div id="calendar-picker">
-          <input type="date" id="datemin" name="datemin"></input>
-          <input type="date" id="datemax" name="datemax"></input>
-          <div id="guest-picker">
-            <h5>GUESTS</h5>
-            <div>1 guest</div>
-          </div>
-        </div>
-        <div>
-          <button id="check-available" onClick={(e) => this.handleClick(e)}>Check availability</button>
-        </div>
-      </div>
+      <Container>
+        <Content>
+          <WidgetHead id="widget-heading">
+            <p id="night-price">${this.props.listingData.standardPrice} / night</p>
+            <Rating id="avg-rating">⭐️ 4.90 (323)</Rating>
+          </WidgetHead>
+          <BookPick id="calendar-picker">
+            <CalPick>
+              <input type="date" id="datemin" name="datemin"></input>
+              <input type="date" id="datemax" name="datemax"></input>
+            </CalPick>
+            <p>GUESTS</p>
+          </BookPick>
+
+          <CheckButton id="check-available" onClick={(e) => this.handleClick(e)}>Check availability</CheckButton>
+        </Content>
+      </Container>
     )
   }
 }
+
+const Container = styled.div`
+  font-family: Poppins;
+  font-weight: 300;
+  float: right;
+  width: 338px;
+  border: 2px solid rgb(221, 221, 221);
+  padding: 24px;
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px;
+  border-radius: 12px;
+`;
+
+const Content = styled.div`
+  display: block;
+  align: center;
+  margin: auto;
+`
+
+const BookPick = styled.div`
+  width: 288px;
+  height: 112px;
+  border: 2px solid rgb(221, 221, 221);
+  border-radius: 12px;
+  padding: 5px;
+  margin: auto;
+`
+const ButtonPick = styled.div`
+  width: 288;
+  margin: auto;
+`
+
+const WidgetHead = styled.div`
+  display: flex;
+`
+
+const Rating = styled.div` 
+  margin-left: 200px;
+  float: right;
+`
+
+const CalPick = styled.div`
+  display: flex;
+`
+
+const CheckButton = styled.button`
+  display: block;
+  margin: auto;
+  margin-top: 20px;
+  width: 298px;
+  height: 48px;
+  background: radial-gradient(#F83158, #D80866);
+  color: rgb(255, 255, 255);
+  border: 2px solid rgba(221, 221, 221, 0.12);
+  border-radius: 12px;
+`
 
 export default Widget
