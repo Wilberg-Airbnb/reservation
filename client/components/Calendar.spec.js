7f9218faf73e3d82e75ebaaa037ffe2678a34880
Sam Johnson <samj@iconcollective.com>
2020-07-10 17:05:59 -0700

    modified unit testing for Calendar

--- client/components/Calendar.spec.js
+++ client/components/Calendar.spec.js
@@ -12,23 +12,26 @@ describe('<Calendar/>', () => {
     var state
 
     beforeEach(() => {
-        // global.window = Object.create(window);
-        // const url = "http://localhost:8888/54";
-        // Object.defineProperty(window, 'location', {
-        //     value: {
-        //         href: url
-        //     }
-        // })
+      global.window = Object.create(window);
+      const url = "http://localhost:8888/54/";
+      Object.defineProperty(window, 'location', {
+        value: {
+          href: url,
+          pathname: '/54/'
+        }
+      })
 
-        // listing = JSON.parse(window.location.href.split('/')[3]);
-        // state = { listingId: listing }
+        listing = JSON.parse(window.location.pathname.slice(1, -1));
+        state = { listingId: listing }
     })
 
-    it('should load with the proper listingId', () => {
+    it('should load with the proper listingId', async (done) => {
 
-        const wrapper = mount(<Calendar/>)
+        const wrapper = mount(<Calendar listingData={state}/>)
 
         
         expect(wrapper.exists()).toBeTruthy();
+        expect(wrapper.props().listingData.listingId).toBe(54);
+        done()
     })
 })
