0c020a1204bbd8f0c8a189b5c6e97a6e9b5309d5
Sam Johnson <samj@iconcollective.com>
2020-07-24 23:19:10 -0700

    added guest picker text/buttons

--- client/components/GuestPicker.jsx
+++ client/components/GuestPicker.jsx
@@ -12,24 +12,89 @@ const GuestContainer = styled.div`
   z-index: 999;
   background: #FFFFFF;
   padding: 16px;
+  display: inline-block;
+`
+
+const GuestSelectLeft = styled.div`
+  width: 20px;
+  height: 20px;
+  border: 1px solid #717171;
+  border-radius: 32px;
+  text-align: center;
+  margin-top: 0px;
+  float: left;
+`
+
+const GuestSelectRight = styled.div`
+  width: 20px;
+  height: 20px;
+  border: 1px solid #717171;
+  border-radius: 32px;
+  text-align: center;
+  margin-top: 0px;
+  float: right;
+`
+
+const GuestFont = styled.p`
+  font-size: 14px;
+  margin-top: 0px;
 `
 
 const GuestCategory = styled.p`
-  font-size: 14px,
+  font-size: 24px;
+`
+const GuestRow = styled.div`
+  width: 100%;
+  height: 32px;
+`
+const GuestNum = styled.div`
+  
+`
+const GuestSpanLeft = styled.span`
+  width: 75px;
+  height: 32px;
+  border: 1px none black;
+  float: left;
+  margin-top: 40px;
+`
+
+const GuestSpanRight = styled.span`
+  width 150px;
+  height: 32px;
+  border: 1px none black;
+  float: right;
+  text-align: center;
+  margin-top: 40px;
+  vertical-align: text-bottom;
 `
 
 const GuestPicker = (props) => {
   return (
     <GuestContainer>
-      <div>
-        <GuestCategory>Adults</GuestCategory>
-      </div>
-      <div>
-        <GuestCategory>Children</GuestCategory>
-      </div>
-      <div>
-        <GuestCategory>Infants</GuestCategory>
-      </div>
+      <GuestSpanLeft>
+        Adults
+      </GuestSpanLeft>
+      <GuestSpanRight>
+        <GuestSelectLeft><GuestFont>-</GuestFont></GuestSelectLeft>
+        5
+        <GuestSelectRight><GuestFont>+</GuestFont></GuestSelectRight>
+      </GuestSpanRight>
+      <GuestSpanLeft>
+        Adults
+      </GuestSpanLeft>
+      <GuestSpanRight>
+        <GuestSelectLeft><GuestFont>-</GuestFont></GuestSelectLeft>
+        2
+        <GuestSelectRight><GuestFont>+</GuestFont></GuestSelectRight>
+      </GuestSpanRight>
+      <GuestSpanLeft>
+        Infants
+      </GuestSpanLeft>
+      <GuestSpanRight>
+        <GuestSelectLeft><GuestFont>-</GuestFont></GuestSelectLeft>
+        1
+        <GuestSelectRight><GuestFont>+</GuestFont></GuestSelectRight>
+      </GuestSpanRight>
     </GuestContainer>
   )
 }
