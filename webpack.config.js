58c1631e08a12e7f4f278fec38f609da21bb421c
Sam Johnson <samj@iconcollective.com>
2020-07-28 10:57:44 -0700

    added minimize to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,30 @@ var SRC_DIR = path.join(__dirname, '/client');
 var DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-    entry: `${SRC_DIR}/index.jsx`,
-    output: {
-      filename: 'bundle.js',
-      path: DIST_DIR
-    },
-    module : {
-      rules : [
-        {
-          test : /\.jsx?/,
-          // include : SRC_DIR,
-          loader : 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
-         }
-        },
-        {
-          test: /\.(png|jpe?g|gif)$/i,
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[ext]'
-          }
+  optimization: {
+    minimize: true
+  },
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
         }
-      ]
-    }
-  };
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      }
+    ]
+  }
+};
